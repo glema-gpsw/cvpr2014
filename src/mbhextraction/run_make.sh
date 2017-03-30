7a17ac5908e833029f6eb8e3cc7e1badd4ba68b7
Gabriel Lema <glema@gopro.com>
2017-03-30 04:12:17 -0700

    cleaned run_make

--- src/mbhextraction/run_make.sh
+++ src/mbhextraction/run_make.sh
@@ -1,4 +1,5 @@
-c++ main.cpp  `python-config --includes` -o mpegflow.o -fPIC -c -D__STDC_CONSTANT_MACROS -lopencv_imgproc -lopencv_core -lswscale -lavdevice -lavformat -lavcodec -lswresample -lavutil -lpthread -lbz2 -lz -lc -lrt -llzma -lva -lboost_python -lpython2.7 -I../../bin/dependencies/include -L../../bin/dependencies/lib
-c++ -o mpegflow.so -shared mpegflow.o -lboost_python -lpython2.7 -lopencv_imgproc -lopencv_core -lswscale -lavdevice -lavformat -lavcodec -lswresample -lavutil -lpthread -lbz2 -lz -lc -lrt -llzma -lva -I../../bin/dependencies/include -L../../bin/dependencies/lib
+INSTALL_DIR=/mnt/hd00/action_fixed_fps_skiing/code/mpegflow/
+c++ main.cpp -Wno-deprecated-declarations  `python-config --includes` -o mpegflow.o -fPIC -c -D__STDC_CONSTANT_MACROS -lopencv_imgproc -lopencv_core -lswscale -lavdevice -lavformat -lavcodec -lswresample -lavutil -lpthread -lz -lc -llzma  -lboost_python -lpython2.7 -I../../bin/dependencies/include -L../../bin/dependencies/lib
+c++ -o mpegflow.so -shared mpegflow.o -lboost_python -lpython2.7 -lopencv_imgproc -lopencv_core -lswscale -lavdevice -lavformat -lavcodec -lswresample -lavutil -lpthread -lz -lc -llzma -I../../bin/dependencies/include -L../../bin/dependencies/lib
 rm -f /mnt/hd00/action_fixed_fps_skiing/code/mpegflow/module_mpegflow.so
-cp mpegflow.so /mnt/hd00/action_fixed_fps_skiing/code/mpegflow/module_mpegflow.so
+cp mpegflow.so $INSTALL_DIR
